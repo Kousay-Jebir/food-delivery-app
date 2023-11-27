a4c085bdb99cbf33c5d511e3c022ac1f1fa66dfa
Iyed <iyedabdelli73@gmail.com>
2023-11-27 12:31:34 +0100

    Add overlay for the cart menu

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,12 +6,18 @@ import CartButtons from './CartElements/CartButtons'
 import './Cart.css'
 
 export default function Cart(props) {
-  return (
+    let cartItems = [
+        { itemName: "Sushi", itemPrice: 11.45, itemQuantity: 2 }
+    ];
+    return (
+    <>
+        <div className={props.state ? 'overlay overlay-visible' : 'overlay overlay hidden'}
+             onClick={() => props.setModal(false)}></div>
     <Container name={props.state? 'cart-modal-block' : 'cart-modal-none'}>
-        <CartItem/>
-        <CartItem/>
-        <TotalAmount/>
-        <CartButtons setModal={props.setModal}/>
+        <CartItem cartItems={cartItems} />
+        <TotalAmount cartItems={cartItems} />
+        <CartButtons setModal={props.setModal} />
     </Container>
-  )
+    </>
+    )
 }
